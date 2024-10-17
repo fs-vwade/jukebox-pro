89477ff951bce4032375b88653c84b738606f908
Victor Wade <p9859817888@gmail.com>
2024-10-16 15:55:08 -1000

    error handler

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const { error404, errorHandler } = require("../middleware");
+
 router.use("/playlists", require("./playlists"));
 router.use("/tracks", require("./tracks"));
 
+router.use(error404);
+
 module.exports = {
 	routes: router,
+	errorHandler,
 };
