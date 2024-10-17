62e5ed6c3970df3ac3c86ff0d6f2571afa8c83ef
Victor Wade <p9859817888@gmail.com>
2024-10-16 21:31:09 -1000

    auth playlists 🔒

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const { authenticate } = require("../api/auth");
 const { error404, errorHandler } = require("../middleware");
 
-router.use("/playlists", require("./playlists"));
+router.use("/playlists", authenticate, require("./playlists")); // this entire path requires authentication
 router.use("/tracks", require("./tracks"));
 
 router.use(error404);
