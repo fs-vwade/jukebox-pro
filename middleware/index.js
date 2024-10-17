f1e0f1ab9cd61435e9c74fe664c8b696c4064600
Victor Wade <p9859817888@gmail.com>
2024-10-16 22:19:43 -1000

    we send a router named errorHandler

--- middleware/index.js
+++ middleware/index.js
@@ -1,14 +1,16 @@
-const error404 = (req, res, next) => {
-	next({ status: 404, message: "Endpoint not found." });
-};
+const express = require("express");
+const router = express.Router();
 
-const errorHandler = async function (err, req, res, next) {
-	console.error(err);
-	res.status(err.status ?? 500);
-	res.json(err.message ?? "Sorry, something broke :(");
-};
+{
+	router.use((req, res, next) => {
+		next({ status: 404, message: "Endpoint not found." });
+	});
 
-module.exports = {
-	error404,
-	errorHandler,
-};
+	router.use((err, req, res, next) => {
+		console.error(err);
+		res.status(err.status ?? 500);
+		res.json(err.message ?? "Sorry, something broke :(");
+	});
+}
+
+module.exports = { errorHandler: router };
