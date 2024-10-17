7b1338f91893bb82e34a297cf049cae1c2917e18
Victor Wade <p9859817888@gmail.com>
2024-10-16 22:11:49 -1000

    playlist routes

--- routes/playlists/index.js
+++ routes/playlists/index.js
@@ -4,8 +4,61 @@ const prisma = require("../../prisma");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {});
-router.get("/:id", async (req, res, next) => {});
-router.post("/", async (req, res, next) => {});
+router.get("/", async (req, res, next) => {
+	try {
+		res.json({
+			playlists: await prisma.playlst.findMany({
+				where: { ownerId: req.customer.id },
+			}),
+		});
+	} catch (e) {
+		next(e);
+	}
+});
+router.get("/:id", async (req, res, next) => {
+	try {
+		const id = Number(req.params.id);
+		const playlist = await prisma.playlist.findUnique({
+			where: { id },
+			include: { tracks: true },
+		});
+
+		if (playlist) {
+			res.json({
+				playlist,
+				tracks: await prisma.track.findMany({
+					where: { id: { in: playlist.tracks } },
+				}),
+			});
+		} else {
+			next({ error: 403, message: "Forbidden access." });
+		}
+	} catch (e) {
+		next(e);
+	}
+});
+router.post("/", async (req, res, next) => {
+	try {
+		const { name, description, trackIds } = req.body;
+
+		if (name && description && Array.isArray(trackIds)) {
+			await prisma.playlist.create({
+				data: {
+					name,
+					description,
+					owner: { connect: { id: req.customer.id } },
+					tracks: {
+						connect: trackIds.map((e) => ({ id: Number(e) })),
+					},
+				},
+			});
+			res.status(201).send(`Playlist created successfully.`);
+		} else {
+			next({ status: 400, message: "Bad request." });
+		}
+	} catch (e) {
+		next(e);
+	}
+});
 
 module.exports = router;
