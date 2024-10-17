1ebe3767ce5f0eb300dff27e495d4d755d6117d7
Victor Wade <p9859817888@gmail.com>
2024-10-16 15:09:07 -1000

    verifyToken dissection and restructuring, willtest

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,26 @@
 const express = require("express");
-const router = express.Router();
+const prisma = require("../prisma");
 
+const router = express.Router();
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.new.JWT_SECRET;
+
+const createToken = (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: "1d" });
+
+const verifyToken = async (req, res, next) => {
+	try {
+		const authHeader = req.headers.authorization;
+		const token = authHeader.split(/(?: |Bearer *)/)[1];
+		if (!token) return next();
+
+		const { id } = jwt.verify(token, JWT_SECRET);
+		req.customer = await prisma.customer.findUniqueOrThrow({
+			where: { id },
+		});
+		next();
+	} catch (e) {
+		next(e);
+	}
+};
+
+router.use(verifyToken);
