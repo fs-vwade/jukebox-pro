dfd901746026bf78eb5b706d8b874cd208a6c2e4
Victor Wade <p9859817888@gmail.com>
2024-10-16 15:54:53 -1000

    quick server config

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const { routes, errorHandler } = require("./routes");
+
+app.use(routes);
+
+app.use(errorHandler);
+
+app.listen(PORT);
