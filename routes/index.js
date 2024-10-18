fc48b8f089bca01b8abb85dd89bb181e3e9e36f7
Victor Wade <p9859817888@gmail.com>
2024-10-17 14:03:21 -1000

    user registration, success

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { authenticate } = require("../api/auth");
+const { authRoutes, authenticate } = require("../api/auth");
 const { errorHandler } = require("../middleware");
+const auth = require("../api/auth");
 
 router.use(require("morgan")("dev"));
 router.use(express.json());
 
+router.use(authRoutes);
 router.use("/playlists", authenticate, require("./playlists")); // this entire path requires authentication
 router.use("/tracks", require("./tracks"));
 
